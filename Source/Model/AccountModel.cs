3270ddd6fdda24fdae1e76827adb5460ed58e8c2
haikeint <haikeint@gmail.com>
2024-08-11 01:31:30 +0700

    feat: add idgen into AccountModel

--- Source/Model/AccountModel.cs
+++ Source/Model/AccountModel.cs
@@ -5,7 +5,7 @@ namespace S84Account.Model
     [Table("account")]
     public class AccountModel
     {
-        public int Id { get; set; }
+        public long Id { get; set; }
         public string Username { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
@@ -13,5 +13,21 @@ namespace S84Account.Model
         public string Address { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Idcode { get; set; } = string.Empty;
+        public static long CreateId(int nodeId = 0) {
+            if(!(nodeId != 0)) nodeId = AccountID.NodeId;
+            return long.Parse(nodeId.ToString() + AccountID.CreateId());
+        }
+
+        private static class AccountID {
+            public static int NodeId = 1000;
+            private static readonly IdGenerator _generator = CreateGenerator();
+            private static IdGenerator CreateGenerator() {
+                DateTime epoch = new (2024, 8, 2, 0, 0, 0, DateTimeKind.Utc);
+                IdStructure idStructure = new (44, 0, 19);
+                IdGeneratorOptions generatorOptions = new (idStructure, new DefaultTimeSource(epoch));
+                return new (0, generatorOptions);
+            }
+            public static string CreateId() => _generator.CreateId().ToString();
+        }
     }
 }
