517987f7789814a4c06fbe5bcc2bf011e0e55d6d
haikeint <haikeint@gmail.com>
2024-09-13 01:55:53 +0700

    feat: send mail for verify after change email

--- Controller/VerifyController.cs
+++ Controller/VerifyController.cs
@@ -1,14 +1,14 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using ACAPI.Data;
 using ACAPI.Service;
 using ACAPI.Helper;
 using Newtonsoft.Json;
+using DotNetEnv;
 
 namespace ACAPI.Controller {
 
     using Microsoft.AspNetCore.Mvc;
     using StackExchange.Redis;
-    using System.Net;
     using System.Security.Principal;
     using System.Text.Json;
 
@@ -34,6 +34,10 @@ namespace ACAPI.Controller {
             var deserializedPayload = JsonConvert.DeserializeObject<dynamic>(identity.Name);
 
             string UserId = deserializedPayload?.Id ?? string.Empty;
+            string Email = deserializedPayload?.Email ?? string.Empty;
+            string NewEmail = deserializedPayload?.NewEmail ?? string.Empty;
+            string Operator = deserializedPayload?.Operator ?? string.Empty;
+            
             string RedisKey = $"{REDIS_VERIFY}{UserId}";
 
             RedisValue[] accountRedis = Redis.GetValue(_redisPool, redisCTX => {
@@ -46,14 +50,66 @@ namespace ACAPI.Controller {
 
             if (accountRedis[0].HasValue) {
                 MysqlContext mysqlContext = _contextFactory.CreateDbContext();
-                string sql = "UPDATE account Set is_email_verified = {0} WHERE id = {1}";
-                Task<int> rowAffect = mysqlContext.Database.ExecuteSqlRawAsync(sql, 1, UserId);
-                if(await rowAffect > 0) {
+                if(!string.IsNullOrEmpty(Operator) && Operator == "RequestChangeEmail") {
+                    if(await SendVerifyEmail(Request.Host.Value, NewEmail, UserId, UserId)) {
+                         string sql = "UPDATE account Set email = {0}, is_email_verified = 0 WHERE id = {1} and email = {2}";
+                        Task<int> rowAffect = mysqlContext.Database.ExecuteSqlRawAsync(sql, NewEmail, UserId, Email);
+                        if(!(await rowAffect > 0)) return BadRequest("Lỗi không xác định");
+                        return Ok($"Vui lòng kiểm tra hòm thư {NewEmail} và xác thực email.");
+                    }
+                    return BadRequest("Lỗi không xác định");
+                } else { 
+                    string sql = "UPDATE account Set is_email_verified = {0} WHERE id = {1}";
+                    Task<int> rowAffect = mysqlContext.Database.ExecuteSqlRawAsync(sql, 1, UserId);
+
+                    if(!(await rowAffect > 0)) return BadRequest("Lỗi không xác định");
                     return Ok("Xác thực Email thành công.");
-                }
-                return BadRequest("Xác thực Email thất bại.");
+                } 
             }
             return BadRequest("Token đã sử dụng.");
+        }
+
+        private async Task<bool> SendVerifyEmail(string host, string email, string userId, string username)
+        { 
+                string redisKey = $"{REDIS_VERIFY}{userId}";
+
+                TimeSpan? ttl = Redis.GetValue(_redisPool, redisCTX => redisCTX.KeyTimeToLive(redisKey));
+
+                if(ttl is not null) return false;
+                
+                var payload = new {
+                    Id = userId,
+                    Email= email,
+                    Operator = "VerifyEmail"
+                };
+               
+                string jwtToken = JWT.GenerateES384(
+                    JsonSerializer.Serialize(payload),
+                    JWT.ISSUER,
+                    host, 
+                    DateTime.UtcNow.AddMinutes(30));
+
+                Redis.Handle(_redisPool, redisCTX =>
+                {
+                    redisCTX.HashSet(redisKey, [
+                        new HashEntry("Token", jwtToken),
+                        ]);
+                    redisCTX.KeyExpire(redisKey, TimeSpan.FromMinutes(30));
+                });
+
+                string verifyLink = $"https://localhost:5000/api/auth/verify-email?token={jwtToken}";
+
+                string emailBody = await _viewRenderService.RenderToStringAsync(
+                    "~/View/VerifyEmailTempalte.cshtml",
+                    new {
+                        UrlStatic = Env.GetString("URL_STATIC"),
+                        Username = username,
+                        Text = "xác thực Email",
+                        ButtonHTML = "Xác thực Email ngay",
+                        Expire = 30,
+                        VerifyLink = verifyLink
+                    });
+                return Mail.Send(email ?? string.Empty, "Xác thực Email tại HBPlay", emailBody);
         }
     }
 }
